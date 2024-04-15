57d67078b63ff714c752800a290681367c183d13
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-04-15 15:53:39 +0000

    sd

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,27 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
-
+        const [name,setName] = useState(
+            {
+                firstName:"",
+                secondName:""
+            }
+        );
         return(
             <div id="main">
                {/* Do not remove the main div */}
+               <input 
+                    placeholder="Enter your first name"
+                    value={name.firstName}
+                    onChange={(e)=>setName({...name,firstName:e.target.value})}
+                />
+               <input 
+                    placeholder="Enter your first name"
+                    value={name.secondName}
+                    onChange={(e)=>setName({...name,secondName:e.target.value})}
+                />
+               <button>Calculate Relationship Future</button>
+               <button>Cleat</button>
             </div>
         )
     }
